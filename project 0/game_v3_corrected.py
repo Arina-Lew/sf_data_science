8d6cbcd56e3d5f7253b53906776204839309aa90
Arina-Lew <96269718+Arina-Lew@users.noreply.github.com>
2022-01-03 01:42:31 +0000

    Update game_v3_corrected.py

--- project 0/game_v3_corrected.py
+++ project 0/game_v3_corrected.py
@@ -7,10 +7,8 @@ import numpy as np
 
 def random_predict(number: int = 1) -> int:
     """Угадываем число
-
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
-
     Returns:
         int: Число попыток
     """
@@ -37,10 +35,8 @@ def random_predict(number: int = 1) -> int:
 
 def score_game(random_predict) -> int:
     """За какое количство попыток в среднем за 10000 подходов угадывает наш алгоритм
-
     Args:
         random_predict ([type]): функция угадывания
-
     Returns:
         int: среднее количество попыток
     """
@@ -54,69 +50,14 @@ def score_game(random_predict) -> int:
     score = int(np.mean(count_ls)) # среднее количество попыток
     
     
-    import numpy as np
-
-
-def game_core_v3(number: int = 1) -> int:
-    """
-    Args:
-        number (int, optional): Загаданное число. Defaults to 1.
-
-    Returns:
-        int: Число попыток
-    """
-    # Ваш код начинается здесь
-
-    attempt_counter = 0   # кол-во попыток
-    prdict_number_min = 1  # нижняя граница поиска числа
-    prdict_number_max = 101  # верхняя граница поиска числа
-    
-    while True:
-        attempt_counter += 1
-        
-        predict_number = (prdict_number_max + prdict_number_min) // 2
-
-        if number > predict_number:
-            prdict_number_min = predict_number  # смещение нижней границы поиска числа
-
-        elif number < predict_number:
-            prdict_number_max = predict_number  # смещение верхней границы поиска числа
-
-        else:
-            break
-
-    # Ваш код заканчивается здесь
-
-    return attempt_counter
-
-def score_game(game_core_v3) -> int:
-    """За какое количество попЫток в среднем за 1000 подходов угадывает наш алгоритм
-    Args:
-        random_predict ([type]): функция угадывания
-    Returns:
-        int: среднее количество попыток
-    """
-
-    count_ls = []  # список для сохранения количества попыток
-    #np.random.seed(1)  # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(10000))  # загадали список чисел
-
-    for number in random_array:
-        count_ls.append(game_core_v3(number))
-
-    score = int(np.mean(count_ls))  # находим среднее количество попыток
-
+   
     print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
     return(score)
 
-score_game(game_core_v3)
+score_game(random_predict)
 
 
 if __name__ == "__main__":
     # RUN
     score_game(random_predict)
-    
-    
-    
-    
     
